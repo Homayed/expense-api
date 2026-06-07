b5aca4f399d4f3203eae81db240777b1c0c1e2cf
Homayed <naser154439@gmail.com>
2026-06-07 23:54:53 +0100

    Add PostgreSQL-backed expense creation

--- main_db.py
+++ main_db.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
 import psycopg2
 from psycopg2.extras import RealDictCursor
+from pydantic import BaseModel
 
 
 
 app = FastAPI()
+class Expense(BaseModel):
+    title:str
+    amount :float
+    category : str
+    paid: bool = False
+
+
 
 def get_connection():
     return psycopg2.connect(
@@ -32,4 +40,22 @@ def get_expenses():
     return {
         "message": "expenses fetched from PostgreSQL",
         "expenses": [dict(row) for row in rows]
+    }
+
+@app.post("/expenses")
+def add_expenses(expense:Expense):
+    connection = get_connection()
+    cursor = connection.cursor(cursor_factory=RealDictCursor)
+    cursor.execute("""
+        INSERT into expenses(title,amount,category,paid)
+        VALUES(%s,%s,%s,%s)
+        RETURNING *;
+    """,(expense.title,expense.amount,expense.category,expense.paid))
+    new_expense = cursor.fetchone()
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return {
+        "message": "expense added successfully",
+        "expense": dict(new_expense)
     }
